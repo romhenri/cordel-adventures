c05595aa290b14ac102cf1eff01dec6d65ecbed9
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-07-01 21:52:47 -0300

    improve: Test objects

--- scripts/test_player.gd
+++ scripts/test_player.gd
@@ -3,9 +3,30 @@ extends CharacterBody2D
 @onready var texture: Sprite2D = get_node("Texture")
 @onready var collision: CollisionShape2D = get_node("Collision")
 
+@export var move_speed: float = 256.0
+
 func _ready():
 	print(texture)
 	print(collision)
+
+func _physics_process(_delta: float) -> void:
+	
+	move()
+
+func move() -> void:
+	var direction: Vector2 = get_direction()
+	# print(direction)
+	velocity = direction * move_speed
+	move_and_slide()
+	
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	#Input.get_axis("move_left", "move_right"),
+	#Input.get_axis("move_up", "move_down")
+	).normalized()
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
