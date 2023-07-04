6d15f06e45ff8e5ed1ef085d70eeefb2a494b49e
Edwirges Vitoria <edwirges.ferreira@dcx.ufpb.br>
2023-07-04 18:19:15 -0300

    improve: Screens

---
+++ scenes/screens/tela_morte.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$TileMap/TileMap/Botoes/tentar_novamente.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
